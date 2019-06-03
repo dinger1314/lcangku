45dbe0eaddd503e2432b27009f334b392205018f
梨涡妹妹_ <51314801+dinger1314@users.noreply.github.com>
2019-06-03 15:35:46 +0800

    Update Test.java

--- 0531/src/作业/Test.java
+++ 0531/src/作业/Test.java
@@ -1,9 +1,9 @@
-package ×÷Òµ;
+package Ã—Ã·Ã’Âµ;
 
 public class Test {
 
 	public static void main(String[] args) {
-		//7.¼ÆËã5!    5*4*3*2*1
+		///    5*4*3*2*1
 		int sum=1;
 		for(int i=5;i>=1;i--) {
 			sum*=i;
